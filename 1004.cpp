39794e1d95c606f5febf1d962da4f3a30d9918e7
westring <95Westring>
2020-02-25 09:15:26 +0800

    add 1004.cpp

---
+++ 1004.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+/*
+排序算法尽量使用stl提供的，提高解题效率 
+*/ 
+
+using namespace std;
+
+typedef struct Student{
+	string name;
+	string id;
+	int score;
+}Student;
+
+int cmp(Student &stu1,Student &stu2){
+	return stu1.score > stu2.score;	
+}
+
+int main(){
+	int num;
+	vector<Student> stus;
+	cin >> num;
+	for(int i = 0;i < num;i++){
+		Student stu;
+		cin >> stu.name >> stu.id >> stu.score;
+		stus.push_back(stu);
+	}
+	//for(vector<Student>::iterator it = stus.begin();it != stus.end();++it){
+	//	cout << (*it).name << (*it).id << endl;
+	//}
+	sort(stus.begin(),stus.end(),cmp);	//vector本身没有sort，故使用全局提供的sort方法 
+	cout << stus.begin()->name << " " << stus.begin()->id << endl;
+	cout << (stus.end()-1)->name << " " << (stus.end()-1)->id << endl;
+	return 0;
+} 
